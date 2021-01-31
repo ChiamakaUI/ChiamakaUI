624f52d24544a9f81aad9cdb63ad621248ca626e
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2021-01-31 10:17:59 +0100

    new change

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,37 +1,40 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import logo from "./Components/logo.png";
+import logo from "./logo.png"
+import './Style.css'
 
 
 
 export default class Navbar extends Component {
     render() {
+
+       
         return (
-            <header className='navbar  navbar-expand-lg navbar-dark px-4' style= {{ backgroundColor:  '#033159' }}>
-                <div className='container-fluid'>
-                    <nav> 
-                        <NavLink to="/" exact>
-                            Home
-                             {/* <img src={logo} alt="Chiamaka Ezemba"/> */}
-                        </NavLink>
+          <header
+            className="navbar navbar-expand-lg navbar-dark header"
+            style={{ backgroundColor: "#16576A" }}
+          >
+            <div className="container-fluid">
+              <nav>
+                <NavLink to="/" exact>
+                  <img src={logo} alt="Chiamaka Ezemba" className="logoStyle" />
+                </NavLink>
+                <div className="d-flex flex-row-reverse d-inline-flex secondDiv">
+                  <NavLink to="/about" className="links p-2">
+                    About
+                  </NavLink>
+                  <NavLink to="/projects" className="links p-2">
+                    Projects
+                  </NavLink>
 
-                        <NavLink to="/projects">
-                            Projects
-                        </NavLink>
-
-                        <NavLink to="/about">
-                            About
-                        </NavLink>
-
-                        <NavLink to="/contact">
-                            Contact Me
-                        </NavLink>
-                    </nav>
-                
-
+                  <NavLink to="/contact" className="links p-2">
+                    Contact Me
+                  </NavLink>
                 </div>
-            </header>
-        )
+              </nav>
+            </div>
+          </header>
+        );
     }
 }
