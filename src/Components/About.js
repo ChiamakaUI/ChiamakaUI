37e1946044455f143bccc2858156060318eb9273
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2021-01-31 13:00:24 +0100

    change

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import "./Style.css";
 
 const About = () => {
     return (
-        <div>
-             <h1>About says Hello</h1>
-        </div>
-    )
+      <div className="container">
+        <h2 className="myName">About Me</h2>
+        <p className="text-center">
+          I am an avid Frontend Developer, quick to grasp new technologies to design efficient and
+          scalable products. Dedicated to optimizing proxies and other services to improve frontend support. Currently seeking an entry-level Software
+          Engineer role to acquire more knowledge on tech stacks. Skilled at team collaboration while working independently in a remote
+          environment.
+        </p>
+      </div>
+    );
 }
 
 export default About;
