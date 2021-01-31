1885cf5d0d832b6e45e35544f3a1eef3d67fe453
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2021-01-31 14:00:42 +0100

    change

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,13 +3,17 @@ import "./Style.css";
 
 const About = () => {
     return (
-      <div className="container">
-        <h2 className="myName">About Me</h2>
-        <p className="text-center">
-          I am an avid Frontend Developer, quick to grasp new technologies to design efficient and
-          scalable products. Dedicated to optimizing proxies and other services to improve frontend support. Currently seeking an entry-level Software
-          Engineer role to acquire more knowledge on tech stacks. Skilled at team collaboration while working independently in a remote
-          environment.
+      <div className="container about">
+        <h2 className="myName  mb-2">About Me</h2>
+        <p className="text-center text-justify aboutPara">
+          I am an avid Frontend Developer, quick to grasp new technologies to
+          design efficient and scalable products. Dedicated to optimizing
+          proxies and other services to improve frontend support. Currently
+          seeking an entry-level Software Engineer role to acquire more
+          knowledge on tech stacks. Skilled at team collaboration while working
+          independently in a remote environment. Over the years, I have
+          developed programming competencies in HTML 5, CSS, JavaScript, React
+          snd Bootstrap through self-learning and online courses.
         </p>
       </div>
     );
