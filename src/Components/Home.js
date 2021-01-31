ef1d1d9b23fae47660055308e43337965924007c
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2021-01-31 12:14:24 +0100

    change

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { NavLink } from "react-router-dom";
+import "./Style.css";
+
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                 <h1>Home says Hello</h1>
-            </div>
-        )
+          <div className="container text-center mt-auto wrapper p-4">
+            <h1 className="myName">Chiamaka Ezemba</h1>
+            <h4 className="bio my-3">
+              I am frontend developer focused on creating clean & user-friendly
+              experience
+            </h4>
+            <button className=" viewBtn btn btn-outline-secondary my-3">
+            <NavLink to="/projects" className="btnLink" >
+                View my work
+              </NavLink>
+            </button>
+          </div>
+        );
     }
 }
